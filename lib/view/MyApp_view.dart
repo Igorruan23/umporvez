4c58634eeb023ce1ed5b31ed38ad9642d9a0bce0
Igorruan23 <115436494+Igorruan23@users.noreply.github.com>
2023-12-24 10:29:02 -0300

    Melhorias na interface

--- lib/view/MyApp_view.dart
+++ lib/view/MyApp_view.dart
@@ -13,72 +13,175 @@ class HomePage extends StatelessWidget {
                 color: Colors.white, fontWeight: FontWeight.w500, fontSize: 25),
           ),
           centerTitle: true,
-          actions: <Widget>[
-            IconButton(onPressed: () {}, icon: Icon(Icons.settings))
-          ],
         ),
         backgroundColor: Colors.blue,
-        body: Stack(children: [
-          Positioned(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.blue, Colors.orange, Colors.deepOrange],
-                  stops: [0.3, 0.7, 0.8],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+        body: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.orange, Colors.deepOrange],
+                    stops: [0.3, 0.7, 0.8],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            child: Container(
-              margin: EdgeInsets.all(90),
-              decoration:
-                  BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.all(90),
+                decoration:
+                    BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+              ),
             ),
-          ),
-          Positioned(
-            child: Container(
-              margin: EdgeInsets.all(100),
-              decoration: BoxDecoration(
-                  color: Colors.orangeAccent, shape: BoxShape.circle),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.all(100),
+                decoration: BoxDecoration(
+                    color: Colors.orangeAccent, shape: BoxShape.circle),
+              ),
             ),
-          ),
-          Positioned(
-            child: Container(
-              margin: EdgeInsets.all(120),
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 253, 194, 116),
-                  shape: BoxShape.circle),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.all(120),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 253, 194, 116),
+                    shape: BoxShape.circle),
+              ),
             ),
-          ),
-          Positioned(
-            child: Container(
-              margin: EdgeInsets.all(150),
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 250, 212, 163),
-                  shape: BoxShape.circle),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.all(150),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 250, 212, 163),
+                    shape: BoxShape.circle),
+              ),
             ),
-          ),
-          Positioned(
-            top: 500.0,
-            left: 25.0,
-            right: 25.0,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              decoration: BoxDecoration(
-                  border:
-                      Border.all(style: BorderStyle.solid, color: Colors.white),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(45),
-                      topRight: Radius.circular(45)),
-                  color: Colors.black),
-              height: 150,
-              width: double.infinity,
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                decoration: BoxDecoration(
+                  color: Colors.black87,
+                ),
+                height: 100,
+                width: double.infinity,
+              ),
             ),
-          )
-        ]),
+            Positioned(
+              top: 30,
+              left: 10,
+              right: 10,
+              child: Container(
+                color: Colors.transparent,
+                alignment: Alignment.center,
+                child: Text(
+                  '00',
+                  style:
+                      GoogleFonts.robotoMono(color: Colors.white, fontSize: 50),
+                ),
+              ),
+            ),
+            Positioned(
+                top: 75,
+                left: 10,
+                right: 10,
+                child: Container(
+                  color: Colors.transparent,
+                  alignment: Alignment.center,
+                  //adicionar um if ternario, se dia for diferente de 0/1 colocar S
+                  child: Text(
+                    'Dias',
+                    style: GoogleFonts.robotoMono(
+                        color: Colors.white, fontSize: 50),
+                  ),
+                )),
+          ],
+        ),
+        //Drawer (lateral menu)
+        drawer: Drawer(
+          backgroundColor: const Color.fromARGB(255, 125, 181, 226),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Vicio",
+                      style: GoogleFonts.robotoMono(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 25),
+                    ),
+                    Text(
+                      "Dias",
+                      style: GoogleFonts.robotoMono(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  "Motivação",
+                  style: GoogleFonts.robotoMono(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                ),
+                leading: Icon(
+                  Icons.rocket,
+                  color: Colors.grey,
+                ),
+                onTap: () {
+                  print('Direcionar');
+                },
+              ),
+              ListTile(
+                title: Text(
+                  "Sobre",
+                  style: GoogleFonts.robotoMono(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                ),
+                leading: Icon(
+                  Icons.info,
+                  color: Colors.grey,
+                ),
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("Informações do programador"),
+                          content: Text(
+                              "Este Aplicativo foi desenvolvido por: IGOR RUAN",
+                              style: GoogleFonts.robotoMono()),
+                          actions: <Widget>[
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Fechar"))
+                          ],
+                        );
+                      });
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
